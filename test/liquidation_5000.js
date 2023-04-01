0e21e984d9378e58b01638f6e35e8ec5eb7cdfaa
kong-wep <pakkapol.20100@gmail.com>
2023-04-01 14:26:22 +0700

    liquidation

--- test/liquidation_5000.js
+++ test/liquidation_5000.js
@@ -3,7 +3,7 @@ const { network, ethers } = require("hardhat");
 const { BigNumber, utils }  = require("ethers");
 const { writeFile } = require('fs');
 
-describe("Liquidation", function () {
+describe("Liquidation 5000", function () {
   it("test", async function () {
     await network.provider.request({
         method: "hardhat_reset",
@@ -25,7 +25,7 @@ describe("Liquidation", function () {
         params: [liquidator],
     }));
 
-    const LiquidationOperator = await ethers.getContractFactory("LiquidationOperator");
+    const LiquidationOperator = await ethers.getContractFactory("Liquidate_5000");
     const liquidationOperator = await LiquidationOperator.deploy(overrides = {gasPrice: gasPrice});
     await liquidationOperator.deployed();
 
